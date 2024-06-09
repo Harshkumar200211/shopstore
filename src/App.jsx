1081d507a3474927846b2d6aff9f39d613b25172
Harsh <shnihardoi2002@gmail.com>
2024-06-09 11:41:33 +0530

    add: initial files

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Product from './Components/Product'
 import Footer from './Components/Footer'
 import Cart from './Components/Cart'
 import Contact from './Components/Contact'
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 function App() {
@@ -51,19 +51,19 @@ function App() {
   return (
     <div>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/contact' element={<Contact/>}></Route>
-          <Route path='/Signin' element={<Signin/>}></Route>
-          <Route path='/Signup' element={<Signup/>}></Route>
-          <Route path='/Cart' element={<Cart  cartItems={cartItems} 
-        removeFromCart={removeFromCart} 
-        increaseQuantity={increaseQuantity} 
-        decreaseQuantity={decreaseQuantity}  />}></Route>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/Signin' element={<Signin />}></Route>
+          <Route path='/Signup' element={<Signup />}></Route>
+          <Route path='/Cart' element={<Cart cartItems={cartItems}
+            removeFromCart={removeFromCart}
+            increaseQuantity={increaseQuantity}
+            decreaseQuantity={decreaseQuantity} />}></Route>
           <Route path='/product' element={<Product addToCart={addToCart} />}></Route>
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </div>
   )
